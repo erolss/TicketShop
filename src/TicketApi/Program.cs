867697d1a8157ed5e0cd771fe9202011f1d85c5d
erolss <erik@eolsson.com>
2018-02-12 21:42:48 +0100

    swagger api

--- src/TicketApi/Program.cs
+++ src/TicketApi/Program.cs
@@ -1,25 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace TicketApi
 {
+    /// <summary>
+    /// Program
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Main
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            var host = new WebHostBuilder()
+                .UseKestrel(options =>
+                {
+                    // options.ThreadCount = 4;
+                    // options.UseHttps("cert.pfx", "certpassword");
+                    options.NoDelay = true;
+                    options.UseConnectionLogging();
+                })
+                .UseUrls("http://+:5000" /*, "https://+:5001" */)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
+
+            host.Run();
+        }
     }
 }
