fc6935eeaf89917ad28053db33b927a1158bf1e0
erolss <erik@eolsson.com>
2018-02-13 23:35:43 +0100

    updated db model, alter tbl add BuyerUserId, BuyerEmail

--- src/DbUp/Scripts/Scripts0004 - alter tbl - add col email.sql
+++ src/DbUp/Scripts/Scripts0004 - alter tbl - add col email.sql
@@ -1,7 +1,8 @@
 /**
-* added column email
+* added column email & UserID
 */
 
 ALTER TABLE TicketTransactions
 	ADD
-		BuyerEmail VARCHAR(255) NOT NULL;
+		BuyerEmail VARCHAR(255) NOT NULL,
+		BuyerUserId VARCHAR(255) NOT NULL UNIQUE;
