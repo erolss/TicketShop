9be739345b4a91cc613f70da771ce30182d3b874
erolss <erik@eolsson.com>
2018-02-14 23:20:15 +0100

    api upgrade dotnet core 2, swagger related

--- src/TicketApi/Program.cs
+++ src/TicketApi/Program.cs
@@ -1,13 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace TicketApi
 {
-    /// <summary>
-    /// Program
-    /// </summary>
     public class Program
     {
+        //public static void Main(string[] args)
+        //{
+        //    BuildWebHost(args).Run();
+        //}
+
+        //public static IWebHost BuildWebHost(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>()
+        //        .Build();
         /// <summary>
         /// Main
         /// </summary>
@@ -19,8 +33,13 @@ namespace TicketApi
                 {
                     // options.ThreadCount = 4;
                     // options.UseHttps("cert.pfx", "certpassword");
-                    options.NoDelay = true;
-                    options.UseConnectionLogging();
+                    options.Listen(IPAddress.Any, 80, listenOptions =>
+                    {
+                        listenOptions.NoDelay = false;
+                        listenOptions.UseConnectionLogging();
+                    });
+                    //options.NoDelay = true;
+                    //options.UseConnectionLogging();
                 })
                 .UseUrls("http://+:5000" /*, "https://+:5001" */)
                 .UseContentRoot(Directory.GetCurrentDirectory())
@@ -29,6 +48,7 @@ namespace TicketApi
                 .Build();
 
             host.Run();
+
         }
     }
 }
